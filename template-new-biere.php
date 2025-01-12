283e69a33434e94edb4d517698009f3f22c0ae78
romeo2705 <romeolm2005@gmail.com>
2025-01-12 12:01:45 +0100

    :SET composant biere 2/3

--- template-new-biere.php
+++ template-new-biere.php
@@ -15,14 +15,14 @@ get_header();
         --secondary: <?= esc_attr($secondary); ?>;
     }
 
-    body{
+    body {
         background-color: var(--primary);
     }
 </style>
 
 <main class="newBiere">
     <section class="hero">
-        <img src="<?php echo esc_url(get_field('hero_img')); ?>" alt="<?= "hero bière" . the_title(); ?>" >
+        <img src="<?php echo esc_url(get_field('hero_img')); ?>" alt="<?= "hero bière" . the_title(); ?>">
         <h1><?php the_title(); ?></h1>
     </section>
 
@@ -34,22 +34,37 @@ get_header();
             <img src="<?php echo esc_url(get_field('beer_img')); ?>" alt="<?= "bière" . the_title(); ?>">
 
             <?php
-                for ($i=0; $i < $nbPicto; $i++) { 
-                    ?>
-                        <div class="composant <?= "composant" . $i +1 ?>">
-                            <div class="line"></div>
-                            <div class="picto">
-                                <img src="<?php echo esc_url(get_field('picto_' . $i +1 . '_img')); ?>" alt="pictogramme d'un composant de la bière">
-                            </div>
-                            <div class="text">
-                                <h3><?php echo esc_textarea(get_field('picto_' .$i +1 . '_title')) ?></h3>
-                                <p><?php echo esc_textarea(get_field('picto_' .$i +1 . '_text')) ?></p>
-                            </div>
-                        </div>
-                    <?php
-                }
+            for ($i = 0; $i < $nbPicto; $i++) {
             ?>
+                <div class="composant <?= "composant" . $i + 1 ?>">
+                    <div class="line"></div>
+                    <div class="picto">
+                        <img src="<?php echo esc_url(get_field('picto_' . $i + 1 . '_img')); ?>" alt="pictogramme d'un composant de la bière">
+                    </div>
+                    <div class="text">
+                        <h3><?php echo esc_textarea(get_field('picto_' . $i + 1 . '_title')) ?></h3>
+                        <p><?php echo esc_textarea(get_field('picto_' . $i + 1 . '_text')) ?></p>
+                    </div>
+                </div>
+            <?php
+            }
+            ?>
+
         </div>
+
+        <?php
+        if (get_field('more_ingredient') == true) {
+        ?>
+            <div class="moreIngredient">
+                <div class="title">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/src/medias/add.svg'); ?>" alt="pictogramme plus">
+                    <h3>ingrédients supplémentaires</h3>
+                </div>
+                <p><?php echo esc_textarea(get_field('more_ingredient_text')) ?></p>
+            </div>
+        <?php
+        }
+        ?>
 
     </section>
 </main>
