214e74b238edccf532e00ceefe96d09f9d9853ca
romeo2705 <romeolm2005@gmail.com>
2025-01-15 21:50:32 +0100

    :ADD page404

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php
+
+get_header();
+?>
+
+<main class="p404">
+    <div class="content">
+        <div class="title">
+
+        </div>
+
+        <a href="/" class="buttonN"></a>
+    </div>
+
+</main>
+
+<?php
+get_footer();
+?>
