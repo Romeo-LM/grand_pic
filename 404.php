2ff6840ad4d1a2c4164d372f4f19d11f7cee1958
romeo2705 <romeolm2005@gmail.com>
2025-01-15 22:10:33 +0100

    :UPDATE fin404+style

--- 404.php
+++ 404.php
@@ -6,11 +6,17 @@ get_header();
 <main class="p404">
     <div class="content">
         <div class="title">
-
+            <h1>oups erreur 404</h1>
+            <h2>vous avez trop bu</h2>
         </div>
 
-        <a href="/" class="buttonN"></a>
+        <a href="/" class="buttonN">
+            <div class="ellipse"></div>
+            <p>Revenir au site</p>
+        </a>
     </div>
+
+    <img src="<?php echo esc_url(get_template_directory_uri() . '/src/medias/beer_wheel.png'); ?>" alt="roue de bières">
 
 </main>
 
