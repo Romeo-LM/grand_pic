adcbde71e50a0eb5fbbc717d071f359d1eb5e86d
t0mataa <thomaspicard23@gmail.com>
2025-01-17 11:30:57 +0100

    add a propos

---
+++ apropos.php
@@ -0,0 +1,284 @@
+<?php
+/* 
+Template Name: À propos
+*/
+
+get_header();
+?>
+
+<main class="apropos">
+    <div class="aproposcat">
+        <div>
+            
+            <img src="<?php echo esc_url(get_template_directory_uri() . '/src/medias/quisommesnouscaps.png'); ?>"
+                alt="capsule de bierre" class="activate">
+        </div>
+        <div>
+            <img src="<?php echo esc_url(get_template_directory_uri() . '/src/medias/nosengagementscaps.png'); ?>"
+                alt="capsule de bierre">
+        </div>
+        <div>
+            <img src="<?php echo esc_url(get_template_directory_uri() . '/src/medias/territoirecaps.png'); ?>"
+                alt="capsule de bierre">
+        </div>
+    </div>
+    <div class="quisommesnous1">
+
+        <div class="quisommesnous2">
+            <div>
+                <h1>
+                    QUI SOMMES NOUS ?
+                </h1>
+                <p>
+                    La Brasserie du Grand Pic, c’est avant tout une aventure humaine portée par quatre passionnés unis
+                    par
+                    leur amour de la bière et de leur terroir. Inspirés par les montagnes qui nous entourent, nous
+                    mettons
+                    tout notre savoir-faire et notre créativité dans chaque recette.
+                    <br><br>
+                    Authenticité, innovation et respect des traditions locales sont au cœur de notre démarche. Que ce
+                    soit
+                    dans nos brassins, nos étiquettes ou nos idées, chaque détail est pensé avec soin pour vous offrir
+                    des
+                    bières uniques et savoureuses.
+                    <br><br>
+                    Rejoignez-nous dans cette aventure, et dégustez le goût de la passion brassée en altitude !
+                </p>
+            </div>
+            <div>
+                <img src="<?php echo esc_url(get_template_directory_uri() . '/src/medias/createurbrasserie.png'); ?>"
+                    alt="Logo brasserie mauriennaise du Grand Pic">
+            </div>
+        </div>
+        <div>
+            <h2>RETROUVEZ NOS BIÈRES PRÈS DE CHEZ VOUS</h2>
+            <iframe src="https://www.google.com/maps/d/embed?mid=1OX1FAHB32SW8tBNiQPi-irvyBLWnUASP" width="100%"
+                height="600"></iframe>
+        </div>
+    </div>
+    <div class="nosengagements ">
+        <div class="noirfondenga">
+            <div class="numenga">
+                <h2>
+                    NOS ENGAGEMENTS
+                </h2>
+                <p>
+                    #1
+                </p>
+            </div>
+            <div class="contenuenga">
+                <div class="imageenga">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/src/medias/engagement1.png'); ?>"
+                        alt="Logo brasserie mauriennaise du Grand Pic">
+                </div>
+                <div class="textenga">
+                    <h3>Notre environnement, notre adn</h3>
+                    <p>Respecter notre environnement et en prendre soin fait partie de notre ADN. C’est pourquoi nos
+                        bières sont composées uniquement de malts locaux et issus de l’agriculture biologique.</p>
+                    <p>Nous visons à moyen terme de produire une partie de nos propres céréales et houblons sur nos
+                        terres situées proches de la brasserie.</p>
+                    <h3>Le local c’est bien, l’ultra local c’est encore mieux !</h3>
+                    <p>Nous travaillons uniquement avec la malterie artisanale Malt’In Pott, basée en Haute Savoie 74.
+                        Leurs céréales sont 100% issues de l’agriculture biologique et proviennent exclusivement de la
+                        région Auvergne Rhône-Alpes dont à 60% du territoire savoyard.</p>
+                    <h3>L’alcool, c’est aussi de l’eau</h3>
+                    <p>Au village d’Epierre, camp de base savoyard de notre brasserie, l’eau est reconnue pour sa grande
+                        pureté.
+                        «Château d’eau» naturel, le Massif de la Lauzière regorge de cette précieuse ressource qui
+                        compose 85% de nos bières.</p>
+                    <h3>Des houblons d'exception pour des bières uniques</h3>
+                    <p>Nos houblons proviennent de filières issues de l’Agriculture Biologique et sont achetés auprès de
+                        revendeurs locaux qui s’approvisionnement en houblons frais.</p>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="numenga">
+                <h2>
+                    NOS ENGAGEMENTS
+                </h2>
+                <p>
+                    #2
+                </p>
+            </div>
+            <div class="contenuenga">
+                <div class="textenga">
+                    <h3>une bière exceptionnelle </h3>
+                    <p>Nous concevons nos bières avec le souci du détail et avec une qualité que nous voulons
+                        maximale d’un point de vue gustatif.</p>
+                    <p>
+                        Notre culture de la bière, couplée à notre riche expérience lorsque nous brassions en
+                        amateurs, nous permettent de proposer des bières racées et affirmées.</p>
+                    <h3>une recette originale </h3>
+                    <p>Dans le garage où ont débuté nos aventures, nous «cuisinons» en permanence de nouvelles
+                        recettes que nous testons dans des petits contenants. Du pire comme au meilleur, nous ressortons
+                        toujours grandis de ces expériences.</p>
+                    <h3>de nombreux tests</h3>
+                    <p>Une fois que nous sommes convaincus que la recette aura toute sa place dans notre gamme, nous
+                        réalisons un test grandeur nature dans notre outil de production pour s’assurer que le goût
+                        reste le même. C’est le moment des (grosses) dégustations !</p>
+                    <h3>Du brassin à l’étiquette</h3>
+                    <p>A l’issu d’un accord final entre nous quatre, nous lançons les brassins qui nous permettront de
+                        servir nos clients. En parallèle, nous concevons l’étiquette pour que notre nouvelle bière
+                        représente fièrement la nature de la Lauzière.</p>
+                </div>
+                <div class="imageenga">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/src/medias/engagement2.png'); ?>"
+                        alt="Logo brasserie mauriennaise du Grand Pic">
+                </div>
+            </div>
+        </div>
+        <div class="noirfondenga">
+            <div class="numenga">
+                <h2>
+                    NOS ENGAGEMENTS
+                </h2>
+                <p>
+                    #3
+                </p>
+            </div>
+            <div class="contenuenga">
+                <div class="imageenga">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/src/medias/engagement3.png'); ?>"
+                        alt="Logo brasserie mauriennaise du Grand Pic">
+                </div>
+                <div class="textenga">
+                    <h3>Une bière au service de la montagne</h3>
+                    <p>Nous sommes convaincus que nos bières seront un puissant outil pédagogique pour apporter des
+                        connaissances à nos clients et à leur entourage.</p>
+                    <p>En véhiculant un esprit proche de la nature et accrocheur au premier regard, nous croyons que
+                        nous
+                        pouvons concilier plaisir de déguster et joie d’apprendre à mieux respecter l’écosystème qui
+                        nous
+                        entoure.</p>
+                    <h3>Des Français de plus en plus engagés</h3>
+                    <p>72% des Français indiquent avoir accru leur intérêt pour ces enjeux écologiques au cours des
+                        derniers
+                        mois.</p>
+                    <p>
+                        Ils sont dans leur ensemble prêts à mettre leurs convictions environnementales en pratique.
+                        Jeunes
+                        (à 65%) et moins jeunes (à 68%) se disent en effet disposés à payer un peu plus cher pour
+                        consommer
+                        des produits écologiques.</p>
+                    <h3>Une brasserie engagée pour l’éco-sensibilisation</h3>
+                    <p>La Brasserie du Grand Pic s’inscrit dans cette démarche pour la production de ses bières. Notre
+                        communication véhiculée sur nos étiquettes a été conçue dans cet esprit.</p>
+                    <p>
+                        Nous sommes aujourd’hui la première brasserie éco-sensibilisatrice de France, qui se met au
+                        service
+                        de son environnement naturel !</p>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="numenga">
+                <h2>
+                    NOS ENGAGEMENTS
+                </h2>
+                <p>
+                    #4
+                </p>
+            </div>
+            <div class="contenuenga">
+                <div class="textenga">
+                    <h3>Rendre à la montagne ce qu’elle nous a donné</h3>
+                    <p>Montagnards depuis la naissance, nous sommes reconnaissants de tout ce qu’elle nous a apporté
+                        émotionnellement, physiquement et spirituellement. </p>
+                    <p>
+                        Nous nous engageons donc à pérenniser l’emploi dans le village de notre enfance, à renforcer
+                        notre activité sur le territoire et à multiplier les initiatives locales pour la sauvegarde du
+                        patrimoine naturel et historique.</p>
+                    <h3>Une brasserie chargée d’histoire</h3>
+                    <p>Nous avons réhabilité un ancien bâtiment manufacturier (dédié à la confection de chaussures)
+                        implanté au cœur du village.</p>
+                    <p>D’apparence vétuste en intérieur et extérieur, nous lui redonnerons ses lettres de noblesse au
+                        fil des années, avec une approche écologiquement responsable.</p>
+                    <h3>Soutenir la nature par nos actions</h3>
+                    <p>Nous nous engageons dès 2023 à reverser 1% de notre chiffre d’affaires aux initiatives
+                        environnementales locales, telles que le Parc Natura 2000 de la Lauzière ou la Mountain Riders
+                        Foundation.</p>
+                    <h3>Soutenir l’agriculture locale</h3>
+                    <p>D’ici 5 ans, nous voulons développer l’autonomie de la brasserie en produisant nos propres
+                        céréales sur nos terres agricoles dans le village ou en partenariat avec des agriculteurs
+                        locaux.</p>
+                    <h3>Durabilité au cœur de nos engagements</h3>
+                    <p>Notre volonté est de nous implanter durablement sur le village et de favoriser notre
+                        développement par des embauches locales.</p>
+                </div>
+                <div class="imageenga">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/src/medias/engagement4.png'); ?>"
+                        alt="Logo brasserie mauriennaise du Grand Pic">
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+    <div class="territoire ">
+        <div class="noirfondenga">
+            <div class="titreterritoire">
+                <h3>LE MASSIF DE LA LAUZIÈRE</h3>
+                <p>Un site d’exception à connaître et à protéger</p>
+            </div>
+            <div class="contenuterritoire">
+                <div class="imageterritoire">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/src/medias/imagelauziere.png'); ?>"
+                        alt="image de la lauzière">
+                </div>
+                <div class="texteterritoire">
+                    <p>Peu nombreux sont ceux qui vivent encore en permanence dans les villages perchés du massif de la
+                        Lauzière, mais ils sont à l’image de cette montagne, d’une force et d’une richesse incroyables.
+                    </p>
+                    <p>Cette sierra alpine n’est surement pas plus belle qu’une autre, mais elle est terriblement
+                        emblématique, vraie et pure.
+                    </p>
+                    <p>Ce territoire NATURA 2000, label de reconnaissance de sa richesse préservée et de son
+                        authenticité, accueille des habitats naturels remarquables ou les espèces animales et végétales
+                        se doivent d’être protégées.</p>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="titreterritoire">
+                <h3>À LA DÉCOUVERTE DES ESPÈCES EMBLÉMATIQUES DU MASSIF DE LA LAUZIÈRE</h3>
+            </div>
+            <div class="contenuterritoire">
+
+                <div class="texteterritoire">
+                    <p>Raymond Destaing est un expert du Massif de la Lauzière.
+                    </p>
+                    <p>
+                        Originaire de Tarentaise sur l’autre versant, il fut pourtant durant de (très) nombreuses années
+                        le garde-forestier du village d’Epierre en Maurienne. Grâce à cette riche expérience, il connaît
+                        les moindres recoins de cette montagne.
+                    </p>
+                    <p>Avez-vous déjà parcouru le Chemin des Droux dans le village et remarquez les panneaux éducatifs
+                        tout au long ? C’est durant son mandat que tout un chacun a pu apprendre à découvrir la richesse
+                        de notre territoire. </p>
+                    <p>
+                        Au travers de mots simples, Raymond transmet son amour de la montagne à quiconque veut bien lui
+                        prêter une oreille attentive. Tout en légèreté, et toujours dans la bonne humeur, il est une
+                        personne remarquable et une figure historique du Massif. </p>
+                    <p>
+                        C’était donc pour nous tout naturel de lui rendre hommage ici , ainsi que de faire appel à ses
+                        précieuses connaissances pour vous faire découvrir plus en détails les espèces qui trônent
+                        fièrement sur nos étiquettes.
+                    </p>
+                    <p>
+                        Raymond, du fond du coeur merci et profites-bien de cette retraite méritée.
+                    </p>
+                </div>
+                <div class="imageterritoire">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/src/medias/raymonddestaing.png'); ?>"
+                        alt="Raymond Destaing">
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
+
+<?php
+get_footer();
+?>
